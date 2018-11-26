1664ed32e437dab2636bc332ca134c71ce2546ac
Vincent Degove <vincent.degove@muage.net>
2018-11-26 15:09:13 +0100

    Add a simple seed with two users and a bike

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+augustin = User.new(email: "cousinaug01@hotmail.fr")
+augustin.password = "123456"
+augustin.save!
+
+laura = User.new(email: "laura.ducosfilippi@gmail.com")
+laura.password = "654321"
+laura.save!
+
+puts "Users Augustin and Laura created!"
+
+velo = Bike.new(name: "Douze V2 de Laura", description: "Un vélo cargo pour transporter plein de choses", price_per_day: "1500", address: "14 rue Crespin du Gast, 75009 Paris")
+velo.user = laura
+velo.save!
+
+puts "One bike created and assignated to the owner Laura"
+
