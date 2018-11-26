5dfd4069f117bc76277b695b3df74902b1328825
augustin cousin <augustincousin.pro@gmail.com>
2018-11-26 16:27:49 +0100

    added add-a-new-bike feature

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,2 +1,20 @@
 class BikesController < ApplicationController
+  def new
+    @bike = Bike.new
+  end
+
+  def create
+     @bike = Bike.new(bikes_params)
+    if @bike.save
+      redirect_to bikes_path(@bike)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bikes_params
+    params.require(:bike).permit(:name, :description, :address, :price_per_day, :photo)
+  end
 end
