c81f6c9278cf57fee77a16ee3d96f5d7edf1b45f
augustin cousin <augustincousin.pro@gmail.com>
2018-11-27 15:17:07 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :bikes, only: [ :new, :create, :show, :index, :edit, :update ] do
-    resources :reservations, only: [ :new, :create, :edit, :show] ]
+    resources :reservations, only: [ :new, :create, :edit, :show]
   end
 end
