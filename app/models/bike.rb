7109148338551c43bc7e664473dcea97c627e743
augustin cousin <augustincousin.pro@gmail.com>
2018-11-29 11:00:22 +0100

    PgSearch setUp

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -10,4 +10,12 @@ class Bike < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  #PgSearch
+  include PgSearch
+  pg_search_scope :search_by_category_and_location,
+    against: [ :title, :syllabus ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
