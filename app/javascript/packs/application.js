10d8b5fdd81e6362104c2581bbe47c30a6d008cf
Vincent Degove <vincent.degove@muage.net>
2018-11-29 16:55:28 +0100

    add the flatpicker date select and tweak things to reactivate webpacker javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,9 @@
+console.log("hey")
 import "bootstrap";
 import { initAddrAutocomplete } from '../plugins/init_addr_autocomplete';
-import "../components/aos";
+// import "../components/aos";
+import "../plugins/flatpickr"
 
 
 initAddrAutocomplete();
+
