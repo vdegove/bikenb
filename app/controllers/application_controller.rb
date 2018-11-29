42762eae967b2372de9d1f5f77cb1403cdd35b9c
Laura Ducos <laura.ducosfilippi@gmail.com>
2018-11-29 13:16:32 +0100

    dashboard, sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,10 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
+  def after_sign_in_path_for(_bike)
+    bikes_path
+  end
+
   private
 
   def skip_pundit?
